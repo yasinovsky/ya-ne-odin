e1697a673a8d2e3f5e455ceabdc1122af9ad12c1
Victor Yasinovsky <yasinovsky@gmail.com>
2026-02-14 18:07:20 +0300

    Common application controller added

---
+++ app/application.php
@@ -0,0 +1,24 @@
+<?php namespace Yaseek\YNO\App;
+
+
+
+/**
+ * Основной класс приложения
+ * @package Yaseek\YNO\App
+ */
+class Application {
+
+
+
+    /**
+     * Возвращает номер версии приложения
+     * @return string
+     */
+    public static function version() {
+        $value = '0.0.0';
+        return $value;
+    }
+
+
+
+}
